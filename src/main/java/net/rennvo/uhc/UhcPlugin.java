c1712e80d557cf4b827ad9981dfd48b8f2cdd610
renNvo <rennvodev@gmail.com>
2022-04-02 15:57:20 +0200

    alpha 0.11

--- src/main/java/net/rennvo/uhc/UhcPlugin.java
+++ src/main/java/net/rennvo/uhc/UhcPlugin.java
@@ -1,8 +1,11 @@
 package net.rennvo.uhc;
 
 import net.rennvo.uhc.command.UhcCommand;
+import net.rennvo.uhc.listener.PlayerJoinListener;
+import net.rennvo.uhc.listener.PlayerQuitListener;
 import net.rennvo.uhc.service.ArenaManager;
 import net.rennvo.uhc.service.UserManager;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class UhcPlugin extends JavaPlugin {
@@ -16,6 +19,9 @@ public final class UhcPlugin extends JavaPlugin {
         this.userManager = new UserManager();
 
         this.getCommand("uhc").setExecutor(new UhcCommand(this, arenaManager, userManager));
+
+        Bukkit.getPluginManager().registerEvents(new PlayerJoinListener(userManager), this);
+        Bukkit.getPluginManager().registerEvents(new PlayerQuitListener(userManager), this);
     }
 
     @Override
